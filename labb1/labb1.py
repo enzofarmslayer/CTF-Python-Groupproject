4f033bf15a4400452a9e814fe670b722e5995f86
Tobias Ulander <tobiasulander@Tobiass-MacBook-Pro.local>
2023-08-28 21:32:27 +0200

    first commit

---
+++ labb1/labb1.py
@@ -0,0 +1,23 @@
+def div_by_three(n):
+    div = n / 3
+    return div == int(div)
+
+
+def max2(x, y):
+    if (x > y):
+        return x
+    return y
+
+def max3(x, y, z):
+    first = max2(x, y)
+    second = max2(first, z)
+    return max2(first, second)
+
+def max(*numbers):
+    ev = numbers[0]
+    for num in numbers:
+        if (ev < num):
+            ev = num
+    return ev
+
+print(max())
